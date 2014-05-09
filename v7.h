23e7dfee75b491a3c4822e7139a6a39f67f8e290
Sergey Lyubka <valenok@gmail.com>
2014-05-09 13:49:06 +0100

    Fixed v7_assign() signature

--- v7.h
+++ v7.h
@@ -74,8 +74,7 @@ void v7_destroy(struct v7 **);
 enum v7_err v7_exec(struct v7 *, const char *source_code);
 enum v7_err v7_exec_file(struct v7 *, const char *path);
 enum v7_err v7_define_func(struct v7 *, const char *name, v7_func_t c_func);
-enum v7_err v7_assign(struct v7 *v7, struct v7_val *obj, struct v7_val *key,
-                      struct v7_val *val);
+enum v7_err v7_assign(struct v7_val *obj, struct v7_val *k, struct v7_val *v);
 void v7_call(struct v7 *v7, struct v7_val *function);
 int v7_sp(struct v7 *v7);
 void v7_init_stdlib(struct v7 *);
