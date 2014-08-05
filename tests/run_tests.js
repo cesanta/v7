fdc591bfdd807fb506bb310bb8b5e6ab1d44e020
Sergey Lyubka <valenok@gmail.com>
2014-08-05 13:21:19 +0100

    Added boilerplate for ecmascript 262 tests

--- tests/run_tests.js
+++ tests/run_tests.js
@@ -5,6 +5,13 @@ load('tests/unit_test.js');
 var testFiles = ['ch09.json'];
 
 for (var i = 0; i < testFiles.length; i++) {
+
   var cfg = load('tests/' + testFiles[i]);
-  printf('Testing ', cfg.testsCollection.name, ' ...');
+  print('Running ECMAScript test: "', cfg.testsCollection.name, '"');
+
+  for (var j = 0; j < cfg.testsCollection.tests.length; j++) {
+    var test = cfg.testsCollection.tests[j];
+    // TODO(lsm): base64-decode test.code and run it.
+    //print(test.code);
+  }
 }
