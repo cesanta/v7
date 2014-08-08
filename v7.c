78159041379c05e1000ada9f4a70a92318b47498
Sergey Lyubka <valenok@gmail.com>
2014-08-08 12:32:53 +0100

    Added base64_encode() and  base64_decode()

--- v7.c
+++ v7.c
@@ -743,6 +743,99 @@ static void Std_exit(struct v7_c_func_arg *cfa) {
   exit(exit_code);
 }
 
+static void base64_encode(const unsigned char *src, int src_len, char *dst) {
+  static const char *b64 =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  int i, j, a, b, c;
+
+  for (i = j = 0; i < src_len; i += 3) {
+    a = src[i];
+    b = i + 1 >= src_len ? 0 : src[i + 1];
+    c = i + 2 >= src_len ? 0 : src[i + 2];
+
+    dst[j++] = b64[a >> 2];
+    dst[j++] = b64[((a & 3) << 4) | (b >> 4)];
+    if (i + 1 < src_len) {
+      dst[j++] = b64[(b & 15) << 2 | (c >> 6)];
+    }
+    if (i + 2 < src_len) {
+      dst[j++] = b64[c & 63];
+    }
+  }
+  while (j % 4 != 0) {
+    dst[j++] = '=';
+  }
+  dst[j++] = '\0';
+}
+
+// Convert one byte of encoded base64 input stream to 6-bit chunk
+static unsigned char from_b64(unsigned char ch) {
+  // Inverse lookup map
+  static const unsigned char tab[128] = {
+    255, 255, 255, 255, 255, 255, 255, 255, //  0
+    255, 255, 255, 255, 255, 255, 255, 255, //  8
+    255, 255, 255, 255, 255, 255, 255, 255, //  16
+    255, 255, 255, 255, 255, 255, 255, 255, //  24
+    255, 255, 255, 255, 255, 255, 255, 255, //  32
+    255, 255, 255,  62, 255, 255, 255,  63, //  40
+     52,  53,  54,  55,  56,  57,  58,  59, //  48
+     60,  61, 255, 255, 255, 200, 255, 255, //  56   '=' is 200, on index 61
+    255,   0,   1,   2,   3,   4,   5,   6, //  64
+      7,   8,   9,  10,  11,  12,  13,  14, //  72
+     15,  16,  17,  18,  19,  20,  21,  22, //  80
+     23,  24,  25, 255, 255, 255, 255, 255, //  88
+    255,  26,  27,  28,  29,  30,  31,  32, //  96
+     33,  34,  35,  36,  37,  38,  39,  40, //  104
+     41,  42,  43,  44,  45,  46,  47,  48, //  112
+     49,  50,  51, 255, 255, 255, 255, 255, //  120
+  };
+  return tab[ch & 127];
+}
+
+static void base64_decode(const unsigned char *s, char *dst) {
+  unsigned char a, b, c, d;
+  while ((a = from_b64(s[0])) != 255 &&
+         (b = from_b64(s[1])) != 255 &&
+         (c = from_b64(s[2])) != 255 &&
+         (d = from_b64(s[3])) != 255) {
+    if (a == 200 || b == 200) break;  // '=' can't be there
+    *dst++ = a << 2 | b >> 4;
+    if (c == 200) break;
+    *dst++ = b << 4 | c >> 2;
+    if (d == 200) break;
+    *dst++ = c << 6 | d;
+    s += 4;
+  }
+  *dst = 0;
+}
+
+static void Std_base64_decode(struct v7_c_func_arg *cfa) {
+  struct v7_val *v = cfa->args[0];
+
+  v7_set_value_type(cfa->result, V7_BOOL);
+  set_empty_string(cfa->result);
+
+  if (cfa->num_args == 1 && v->type == V7_STR && v->v.str.len > 0) {
+    cfa->result->v.str.len = v->v.str.len * 2 / 3 + 1;
+    cfa->result->v.str.buf = malloc(cfa->result->v.str.len + 1);
+    base64_decode((const unsigned char *) v->v.str.buf, cfa->result->v.str.buf);
+  }
+}
+
+static void Std_base64_encode(struct v7_c_func_arg *cfa) {
+  struct v7_val *v = cfa->args[0];
+
+  v7_set_value_type(cfa->result, V7_BOOL);
+  set_empty_string(cfa->result);
+
+  if (cfa->num_args == 1 && v->type == V7_STR && v->v.str.len > 0) {
+    cfa->result->v.str.len = v->v.str.len * 3 / 2 + 1;
+    cfa->result->v.str.buf = malloc(cfa->result->v.str.len + 1);
+    base64_encode((const unsigned char *) v->v.str.buf, v->v.str.len,
+                  cfa->result->v.str.buf);
+  }
+}
+
 static void init_stdlib(void) {
   static int prototypes_initialized;
   if (prototypes_initialized) return;
@@ -794,6 +887,8 @@ static void init_stdlib(void) {
   SET_RO_PROP(s_global, "print", V7_C_FUNC, c_func, Std_print);
   SET_RO_PROP(s_global, "exit", V7_C_FUNC, c_func, Std_exit);
   SET_RO_PROP(s_global, "load", V7_C_FUNC, c_func, Std_load);
+  SET_RO_PROP(s_global, "base64_encode", V7_C_FUNC, c_func, Std_base64_encode);
+  SET_RO_PROP(s_global, "base64_decode", V7_C_FUNC, c_func, Std_base64_decode);
 
   SET_RO_PROP2(s_global, "Object", V7_C_FUNC, &s_object, c_func, Object_ctor);
   SET_RO_PROP2(s_global, "Number", V7_C_FUNC, &s_number, c_func, Number_ctor);
