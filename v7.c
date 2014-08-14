562872c828b0b4334504aa27e8e841a42cd37be6
M <m.wannatheapp@gmail.com>
2014-08-14 00:50:13 -0400

    Add support for concatenation of strings and objects

--- v7.c
+++ v7.c
@@ -1626,9 +1626,23 @@ static void init_stdlib(void) {
   v7_set_class(&s_global, V7_CLASS_OBJECT);
   s_global.ref_count = 1;
 }
-static enum v7_err arith(struct v7_val *a, struct v7_val *b,
+static enum v7_err arith(struct v7 *v7, struct v7_val *a, struct v7_val *b,
                          struct v7_val *res, int op) {
-  if (a->type == V7_TYPE_STR && b->type == V7_TYPE_STR && op == '+') {
+  if (a->type == V7_TYPE_STR && op == '+') {
+    struct v7_val b_str;
+    if (b->type != V7_TYPE_STR) {
+      // When b is not a String, try using b.toString().
+      struct v7_val *toString;
+      toString = v7_lookup(b, "toString");
+      if (toString == NULL) {
+        return V7_TYPE_ERROR;
+      }
+      struct v7_c_func_arg arg = {
+        v7, b, &b_str, NULL, 0, 0
+      };
+      toString->v.c_func(&arg);
+      b = &b_str;
+    }
     char *str = (char *) malloc(a->v.str.len + b->v.str.len + 1);
     CHECK(str != NULL, V7_OUT_OF_MEMORY);
     v7_init_str(res, str, a->v.str.len + b->v.str.len, 0);
@@ -1660,7 +1674,7 @@ static enum v7_err do_arithmetic_op(struct v7 *v7, int op, int sp1, int sp2) {
   CHECK(v7->sp >= 2, V7_STACK_UNDERFLOW);
 
   memset(&tmp, 0, sizeof(tmp));
-  TRY(arith(v1, v2, &tmp, op));
+  TRY(arith(v7, v1, v2, &tmp, op));
   res = make_value(v7, tmp.type);
   CHECK(res != NULL, V7_OUT_OF_MEMORY);
   res->v = tmp.v;
@@ -2379,12 +2393,12 @@ static enum v7_err parse_assign(struct v7 *v7, struct v7_val *obj, int op) {
         CHECK(v7->sp >= 2, V7_INTERNAL_ERROR);
         TRY(v7_setv(v7, obj, V7_TYPE_STR, V7_TYPE_OBJ, tok, tok_len, 1, b));
         return V7_OK;
-      case OP_PLUS_ASSIGN: TRY(arith(a, b, a, '+')); break;
-      case OP_MINUS_ASSIGN: TRY(arith(a, b, a, '-')); break;
-      case OP_MUL_ASSIGN: TRY(arith(a, b, a, '*')); break;
-      case OP_DIV_ASSIGN: TRY(arith(a, b, a, '/')); break;
-      case OP_REM_ASSIGN: TRY(arith(a, b, a, '%')); break;
-      case OP_XOR_ASSIGN: TRY(arith(a, b, a, '^')); break;
+      case OP_PLUS_ASSIGN: TRY(arith(v7, a, b, a, '+')); break;
+      case OP_MINUS_ASSIGN: TRY(arith(v7, a, b, a, '-')); break;
+      case OP_MUL_ASSIGN: TRY(arith(v7, a, b, a, '*')); break;
+      case OP_DIV_ASSIGN: TRY(arith(v7, a, b, a, '/')); break;
+      case OP_REM_ASSIGN: TRY(arith(v7, a, b, a, '%')); break;
+      case OP_XOR_ASSIGN: TRY(arith(v7, a, b, a, '^')); break;
       default: return V7_NOT_IMPLEMENTED;
     }
     TRY(inc_stack(v7, -1));
