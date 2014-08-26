b5ce621fe94ec9ced0215a431f42fd3ebc4beac1
Sergey Lyubka <valenok@gmail.com>
2014-08-27 00:23:06 +0100

    Tokenizer enhanced

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -12,7 +12,14 @@ enum v7_tok {
   // Punctuators
   TOK_OPEN_CURLY, TOK_CLOSE_CURLY, TOK_OPEN_PAREN, TOK_CLOSE_PAREN,
   TOK_OPEN_BRACKET, TOK_CLOSE_BRACKET, TOK_DOT, TOK_COLON, TOK_SEMICOLON,
-  TOK_COMMA, TOK_EQ_EQ, TOK_EQ, TOK_ASSIGN,
+  TOK_COMMA, TOK_EQ_EQ, TOK_EQ, TOK_ASSIGN, TOK_NE, TOK_NE_NE, TOK_NOT,
+  TOK_REM_ASSIGN, TOK_REM, TOK_MUL_ASSIGN, TOK_MUL, TOK_DIV_ASSIGN, TOK_DIV,
+  TOK_XOR_ASSIGN, TOK_XOR, TOK_PLUS_PLUS, TOK_PLUS_ASSING, TOK_PLUS,
+  TOK_MINUS_MINUS, TOK_MINUS_ASSING, TOK_MINUS,
+  TOK_LOGICAL_AND, TOK_LOGICAL_AND_ASSING, TOK_AND,
+  TOK_LOGICAL_OR, TOK_LOGICAL_OR_ASSING, TOK_OR,
+  TOK_QUESTION, TOK_TILDA, TOK_LE, TOK_LT, TOK_GE, TOK_GT,
+  TOK_LSHIFT_ASSIGN, TOK_LSHIFT, TOK_RSHIFT_ASSIGN, TOK_RSHIFT,
 
   // Keywords
   TOK_BREAK, TOK_CASE, TOK_CATCH, TOK_CONTINUE, TOK_DEBUGGER, TOK_DEFAULT,
@@ -21,7 +28,7 @@ enum v7_tok {
   TOK_THIS, TOK_THROW, TOK_TRUE, TOK_TRY, TOK_TYPEOF, TOK_UNDEFINED, TOK_VAR,
   TOK_VOID, TOK_WHILE, TOK_WITH,
 
-  // TODO(lsm): process reserved words too
+  // TODO(lsm): process these reserved words too
   TOK_CLASS, TOK_ENUM, TOK_EXTENDS, TOK_SUPER, TOK_CONST, TOK_EXPORT,
   TOK_IMPORT, TOK_IMPLEMENTS, TOK_LET, TOK_PRIVATE, TOK_PUBLIC,
   TOK_INTERFACE, TOK_PACKAGE, TOK_PROTECTED, TOK_STATIC, TOK_YIELD,
@@ -66,29 +73,12 @@ static int skip_to_next_tok(const char **ptr) {
   return num_lines;
 }
 
-#if 0
-static const char *s_punctuators[] = {
-  "{", "}", "(", ")", "[", "]", ".", ";", ",",
-
-  "<", ">", "<=", ">=", "==",
-  "!=", "===", "!==", "+", "-", "*", "%", "++", "--", "<<", ">>", ">>>", "&",
-  "|", "^", "!", "~", "&&", "||", "?", ":", "=", "+=", "-=", "*=", "%=", "<<=",
-  ">>=", ">>>=", "&=", "|=", "^=", "/", "/="
-};
-#endif
-
 static int is_valid_identifier_char(int ch) {
   return ch == '$' || ch == '_' || isalnum(ch);
 }
 
-static enum v7_tok ident(const char *src, struct v7_vec *vec) {
+static void ident(const char *src, struct v7_vec *vec) {
   while (is_valid_identifier_char((unsigned char) src[vec->len])) vec->len++;
-  return TOK_IDENTIFIER;
-}
-
-static int eq(const char *src, struct v7_vec *vec, const char *s, int len) {
-  ident(src, vec);
-  return len == vec->len && memcmp(src + 1, s + 1, len - 1) == 0;
 }
 
 static enum v7_tok kw(const struct v7_vec *vec, ...) {
@@ -96,7 +86,7 @@ static enum v7_tok kw(const struct v7_vec *vec, ...) {
   va_list ap;
 
   va_start(ap, vec);
-  while ((tok = va_arg(ap, int)) > 0) {
+  while ((tok = (enum v7_tok) va_arg(ap, int)) > 0) {
     const struct v7_vec *k = &s_keywords[tok - TOK_BREAK];
     if (k->len == vec->len &&
       memcmp(vec->p + 1, k->p + 1, k->len - 1) == 0) break;
@@ -107,45 +97,116 @@ static enum v7_tok kw(const struct v7_vec *vec, ...) {
   return tok > 0 ? tok : TOK_IDENTIFIER;
 }
 
+static enum v7_tok punct1(const char *s, struct v7_vec *vec,
+  int ch1, enum v7_tok tok1, enum v7_tok tok2) {
+
+  if (s[1] == ch1) {
+    vec->len++;
+    return tok1;
+  }
+
+  return tok2;
+}
+
+static enum v7_tok punct2(const char *s, struct v7_vec *vec,
+  int ch1, enum v7_tok tok1, int ch2, enum v7_tok tok2, enum v7_tok tok3) {
+
+  if (s[1] == ch1 && s[2] == ch2) {
+    vec->len += 2;
+    return tok2;
+  }
+
+  return punct1(s, vec, ch1, tok1, tok3);
+}
+
+static enum v7_tok punct3(const char *s, struct v7_vec *vec,
+  int ch1, enum v7_tok tok1, int ch2, enum v7_tok tok2, enum v7_tok tok3) {
+
+  if (s[1] == ch1) {
+    vec->len++;
+    return tok1;
+  } else if (s[1] == ch2) {
+    vec->len++;
+    return tok2;
+  }
+  return tok3;
+}
+
+
 V7_PRIVATE enum v7_tok get_next_token(const char *s, struct v7_vec *vec) {
   vec->p = s;
   vec->len = 1;
 
   switch (s[0]) {
-    case '=': return eq(s, vec, "===", 3) ? TOK_EQ_EQ :
-    eq(s, vec, "==", 2) ? TOK_EQ : TOK_ASSIGN;
-
     // Letters
     case 'a': ident(s, vec); return TOK_IDENTIFIER;
     case 'b': ident(s, vec); return kw(vec, TOK_BREAK, 0);
     case 'c': ident(s, vec); return kw(vec, TOK_CATCH, TOK_CASE, 0);
-    case 'd': ident(s, vec); return kw(vec, TOK_DEBUGGER, TOK_DEFAULT, TOK_DELETE, TOK_DO, 0);
+    case 'd': ident(s, vec); return kw(vec, TOK_DEBUGGER, TOK_DEFAULT,
+                                       TOK_DELETE, TOK_DO, 0);
     case 'e': ident(s, vec); return kw(vec, TOK_ELSE, 0);
-    case 'f': ident(s, vec); return kw(vec, TOK_FALSE, TOK_FINALLY, TOK_FOR, TOK_FUNCTION, 0);
-    case 'g': return ident(s, vec);
-    case 'h': return ident(s, vec);
+    case 'f': ident(s, vec); return kw(vec, TOK_FALSE, TOK_FINALLY, TOK_FOR,
+                                       TOK_FUNCTION, 0);
+    case 'g':
+    case 'h': ident(s, vec); return TOK_IDENTIFIER;
     case 'i': ident(s, vec); return kw(vec, TOK_IF, TOK_IN, TOK_INSTANCEOF, 0);
-    case 'j': return ident(s, vec);
-    case 'k': return ident(s, vec);
-    case 'l': return ident(s, vec);
-    case 'm': return ident(s, vec);
+    case 'j':
+    case 'k':
+    case 'l':
+    case 'm': ident(s, vec); return TOK_IDENTIFIER;
     case 'n': ident(s, vec); return kw(vec, TOK_NEW, TOK_NULL, 0);
-    case 'o': return ident(s, vec);
-    case 'p': return ident(s, vec);
-    case 'q': return ident(s, vec);
+    case 'o':
+    case 'p':
+    case 'q': ident(s, vec); return TOK_IDENTIFIER;
     case 'r': ident(s, vec); return kw(vec, TOK_RETURN, 0);
     case 's': ident(s, vec); return kw(vec, TOK_SWITCH, 0);
-    case 't': ident(s, vec); return kw(vec, TOK_THIS, TOK_THROW, TOK_TRUE, TOK_TRY, TOK_TYPEOF, 0);
+    case 't': ident(s, vec); return kw(vec, TOK_THIS, TOK_THROW, TOK_TRUE,
+                                       TOK_TRY, TOK_TYPEOF, 0);
     case 'u': ident(s, vec); return kw(vec, TOK_UNDEFINED, 0);
     case 'v': ident(s, vec); return kw(vec, TOK_VAR, TOK_VOID, 0);
     case 'w': ident(s, vec); return kw(vec, TOK_WHILE, TOK_WITH, 0);
-    case 'x': return ident(s, vec);
-    case 'y': return ident(s, vec);
-    case 'z': return ident(s, vec);
+    case 'x':
+    case 'y':
+    case 'z': ident(s, vec); return TOK_IDENTIFIER;
 
-    // Digits
+    case '_': case '$':
+    case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G':
+    case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':
+    case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':
+    case 'V': case 'W': case 'X': case 'Y':
+    case 'Z': ident(s, vec); return TOK_IDENTIFIER;
+
+    // Numbers
     case '0': case '1': case '2': case '3': case '4': case '5':
-    case '6': case '7': case '8': case '9': return TOK_NUMBER;
+    case '6': case '7': case '8':
+    case '9': return TOK_NUMBER;
+
+    // String literals
+
+    // Punctuators
+    case '=': return punct2(s, vec, '=', TOK_EQ, '=', TOK_EQ_EQ, TOK_ASSIGN);
+    case '!': return punct2(s, vec, '=', TOK_NE, '=', TOK_NE_NE, TOK_NOT);
+
+    case '%': return punct1(s, vec, '=', TOK_REM_ASSIGN, TOK_REM);
+    case '*': return punct1(s, vec, '=', TOK_MUL_ASSIGN, TOK_MUL);
+    case '/': return punct1(s, vec, '=', TOK_DIV_ASSIGN, TOK_DIV);
+    case '^': return punct1(s, vec, '=', TOK_XOR_ASSIGN, TOK_XOR);
+
+    case '+': return punct3(s, vec, '+', TOK_PLUS_PLUS, '=',
+                            TOK_PLUS_ASSING, TOK_PLUS);
+    case '-': return punct3(s, vec, '-', TOK_MINUS_MINUS, '=',
+                            TOK_MINUS_ASSING, TOK_MINUS);
+    case '&': return punct3(s, vec, '&', TOK_LOGICAL_AND, '=',
+                            TOK_LOGICAL_AND_ASSING, TOK_AND);
+    case '|': return punct3(s, vec, '|', TOK_LOGICAL_OR, '=',
+                            TOK_LOGICAL_OR_ASSING, TOK_OR);
+
+    case '<':
+      if (s[1] == '=') { vec->len = 2; return TOK_LE; }
+      return punct2(s, vec, '<', TOK_LSHIFT, '=', TOK_LSHIFT_ASSIGN, TOK_LT);
+    case '>':
+      if (s[1] == '=') { vec->len = 2; return TOK_GE; }
+      return punct2(s, vec, '<', TOK_RSHIFT, '=', TOK_RSHIFT_ASSIGN, TOK_GT);
 
     case '{': return TOK_OPEN_CURLY;
     case '}': return TOK_CLOSE_CURLY;
@@ -156,6 +217,8 @@ V7_PRIVATE enum v7_tok get_next_token(const char *s, struct v7_vec *vec) {
     case '.': return TOK_DOT;
     case ';': return TOK_SEMICOLON;
     case ':': return TOK_COLON;
+    case '?': return TOK_QUESTION;
+    case '~': return TOK_TILDA;
 
     default: vec->len = 0; return TOK_END_OF_INPUT;
   }
