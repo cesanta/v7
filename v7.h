3cf8db168164e0d9b58c49a02184a73a3c01ada1
Sergey Lyubka <valenok@gmail.com>
2014-10-28 12:56:47 +0000

    Re-formatted v7.h according to Google style

--- v7.h
+++ v7.h
@@ -17,7 +17,7 @@
  */
 
 #ifndef V7_HEADER_INCLUDED
-#define  V7_HEADER_INCLUDED
+#define V7_HEADER_INCLUDED
 
 #ifdef __cplusplus
 extern "C" {
@@ -55,22 +55,43 @@ typedef signed char sint8_t;
 
 #ifndef V7_EX_TRY_CATCH
 #define V7_EX_TRY_CATCH(catch_point) setjmp(catch_point)
-#define V7_EX_THROW(c, m, message) do{m = message; longjmp(c, 1);}while(0)
+#define V7_EX_THROW(c, m, message) \
+  do {                             \
+    m = message;                   \
+    longjmp(c, 1);                 \
+  } while (0)
 #endif
 
-struct v7;      /* Opaque structure. V7 engine handler. */
-struct v7_val;  /* Opaque structure. Holds V7 value, which has v7_type type. */
+struct v7;     /* Opaque structure. V7 engine handler. */
+struct v7_val; /* Opaque structure. Holds V7 value, which has v7_type type. */
 
 enum v7_type {
-  V7_TYPE_UNDEF, V7_TYPE_NULL, V7_TYPE_BOOL, V7_TYPE_STR, V7_TYPE_NUM,
-  V7_TYPE_OBJ, V7_NUM_TYPES
+  V7_TYPE_UNDEF,
+  V7_TYPE_NULL,
+  V7_TYPE_BOOL,
+  V7_TYPE_STR,
+  V7_TYPE_NUM,
+  V7_TYPE_OBJ,
+  V7_NUM_TYPES
 };
 
 enum v7_err {
-  V7_OK, V7_ERROR, V7_EVAL_ERROR, V7_RANGE_ERROR, V7_REFERENCE_ERROR,
-  V7_SYNTAX_ERROR, V7_TYPE_ERROR, V7_URI_ERROR,
-  V7_OUT_OF_MEMORY, V7_INTERNAL_ERROR, V7_STACK_OVERFLOW, V7_STACK_UNDERFLOW,
-  V7_CALLED_NON_FUNCTION, V7_NOT_IMPLEMENTED, V7_STR_TOO_LONG, V7_REGEXP_ERROR,
+  V7_OK,
+  V7_ERROR,
+  V7_EVAL_ERROR,
+  V7_RANGE_ERROR,
+  V7_REFERENCE_ERROR,
+  V7_SYNTAX_ERROR,
+  V7_TYPE_ERROR,
+  V7_URI_ERROR,
+  V7_OUT_OF_MEMORY,
+  V7_INTERNAL_ERROR,
+  V7_STACK_OVERFLOW,
+  V7_STACK_UNDERFLOW,
+  V7_CALLED_NON_FUNCTION,
+  V7_NOT_IMPLEMENTED,
+  V7_STR_TOO_LONG,
+  V7_REGEXP_ERROR,
   V7_NUM_ERRORS
 };
 
@@ -84,16 +105,15 @@ struct v7_c_func_arg {
 };
 typedef enum v7_err (*v7_func_t)(struct v7_c_func_arg *arg);
 
+struct v7 *v7_create(void);    /* Creates and initializes V7 engine */
+void v7_destroy(struct v7 **); /* Cleanes up and deallocates V7 engine */
 
-struct v7 *v7_create(void);       /* Creates and initializes V7 engine */
-void v7_destroy(struct v7 **);    /* Cleanes up and deallocates V7 engine */
+struct v7_val *v7_exec(struct v7 *, const char *str); /* Executes string */
+struct v7_val *v7_exec_file(struct v7 *, const char *path); /* Executes file */
 
-struct v7_val *v7_exec(struct v7 *, const char *str);         /* Executes string */
-struct v7_val *v7_exec_file(struct v7 *, const char *path);   /* Executes file */
-
-struct v7_val *v7_global(struct v7 *);  /* Returns global obj (root namespace) */
+struct v7_val *v7_global(struct v7 *); /* Returns global obj (root namespace) */
 char *v7_stringify(const struct v7_val *v, char *buf, int bsiz);
-const char *v7_get_error_string(const struct v7 *);  /* Returns error string */
+const char *v7_get_error_string(const struct v7 *); /* Returns error string */
 int v7_is_true(const struct v7_val *);
 void v7_copy(struct v7 *v7, struct v7_val *from, struct v7_val *to);
 
