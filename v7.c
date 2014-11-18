583fe4d24e1a06f7280fc298c6ace7b0d2de3eff
vrz <vrz@mail.ru>
2014-11-18 13:04:02 +0300

    v7_stringify() corrected for negative integer numbers

--- v7.c
+++ v7.c
@@ -7997,7 +7997,7 @@ char *v7_stringify(const struct v7_val *v, char *buf, int bsiz) {
     if (v->v.num > ((uint64_t)1 << 52) || ceil(v->v.num) != v->v.num) {
       snprintf(buf, bsiz, "%lg", v->v.num);
     } else {
-      snprintf(buf, bsiz, "%lu", (unsigned long)v->v.num);
+      snprintf(buf, bsiz, "%ld", (unsigned long)v->v.num);
     }
   } else if (is_string(v)) {
     snprintf(buf, bsiz, "%.*s", (int)v->v.str.len, v->v.str.buf);
