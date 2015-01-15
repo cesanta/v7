eeda115648d40d3fc92f70e9e933f73c29924087
Marko Mikulicic <mkm@cesanta.com>
2015-01-15 18:07:56 +0100

    Implement `Array` constructor and methods

--- src/array.c
+++ src/array.c
@@ -5,12 +5,23 @@
 
 #include "internal.h"
 
-V7_PRIVATE enum v7_err Array_ctor(struct v7_c_func_arg *cfa) {
-  struct v7_val *obj =
-      cfa->called_as_constructor ? cfa->this_obj : v7_push_new_object(cfa->v7);
-  v7_set_class(obj, V7_CLASS_ARRAY);
-  return V7_OK;
+V7_PRIVATE val_t Array_ctor(struct v7 *v7, val_t this_obj, val_t args) {
+  (void) v7;
+  (void) this_obj;
+  return args;
 }
+
+V7_PRIVATE val_t Array_push(struct v7 *v7, val_t this_obj, val_t args) {
+  val_t v = V7_UNDEFINED;
+  int i, len = v7_array_length(v7, args);
+  for (i = 0; i < len; i++) {
+    v = v7_array_at(v7, args, i);
+    v7_array_append(v7, this_obj, v);
+  }
+  return v;
+}
+
+#if 0
 V7_PRIVATE void Arr_length(struct v7_val *this_obj, struct v7_val *arg,
                            struct v7_val *result) {
   struct v7_prop *p;
@@ -19,14 +30,6 @@ V7_PRIVATE void Arr_length(struct v7_val *this_obj, struct v7_val *arg,
   for (p = this_obj->v.array; p != NULL; p = p->next) {
     result->v.num += 1.0;
   }
-}
-
-V7_PRIVATE enum v7_err Arr_push(struct v7_c_func_arg *cfa) {
-  int i;
-  for (i = 0; i < cfa->num_args; i++) {
-    v7_append(cfa->v7, cfa->this_obj, cfa->args[i]);
-  }
-  return V7_OK;
 }
 
 V7_PRIVATE int cmp_prop(const void *pa, const void *pb) {
@@ -57,13 +60,11 @@ V7_PRIVATE enum v7_err Arr_sort(struct v7_c_func_arg *cfa) {
   free(arr);
   return V7_OK;
 }
+#endif
 
-V7_PRIVATE void init_array(void) {
-  init_standard_constructor(V7_CLASS_ARRAY, Array_ctor);
-
-  SET_PROP_FUNC(s_prototypes[V7_CLASS_ARRAY], "length", Arr_length);
-  SET_METHOD(s_prototypes[V7_CLASS_ARRAY], "push", Arr_push);
-  SET_METHOD(s_prototypes[V7_CLASS_ARRAY], "sort", Arr_sort);
-
-  SET_RO_PROP_V(s_global, "Array", s_constructors[V7_CLASS_ARRAY]);
+V7_PRIVATE void init_array(struct v7 *v7) {
+  v7_set_property(v7, v7->global_object, "Array", 5, 0,
+                  v7_create_cfunction(Array_ctor));
+  v7_set_property(v7, v7->array_prototype, "push", 4, 0,
+                  v7_create_cfunction(Array_push));
 }
--- v7.c
+++ v7.c
@@ -867,6 +867,7 @@ v7_cfunction_t val_to_cfunction(val_t);
 const char *val_to_string(struct v7 *, val_t *, size_t *);
 
 V7_PRIVATE void init_object(struct v7 *v7);
+V7_PRIVATE void init_array(struct v7 *v7);
 V7_PRIVATE void init_error(struct v7 *v7);
 V7_PRIVATE void init_boolean(struct v7 *v7);
 
@@ -3170,6 +3171,75 @@ int main(void) {
  */
 
 
+V7_PRIVATE val_t Array_ctor(struct v7 *v7, val_t this_obj, val_t args) {
+  (void) v7;
+  (void) this_obj;
+  return args;
+}
+
+V7_PRIVATE val_t Array_push(struct v7 *v7, val_t this_obj, val_t args) {
+  val_t v = V7_UNDEFINED;
+  int i, len = v7_array_length(v7, args);
+  for (i = 0; i < len; i++) {
+    v = v7_array_at(v7, args, i);
+    v7_array_append(v7, this_obj, v);
+  }
+  return v;
+}
+
+#if 0
+V7_PRIVATE void Arr_length(struct v7_val *this_obj, struct v7_val *arg,
+                           struct v7_val *result) {
+  struct v7_prop *p;
+  if (NULL == result || arg) return;
+  v7_init_num(result, 0.0);
+  for (p = this_obj->v.array; p != NULL; p = p->next) {
+    result->v.num += 1.0;
+  }
+}
+
+V7_PRIVATE int cmp_prop(const void *pa, const void *pb) {
+  const struct v7_prop *p1 = *(struct v7_prop **) pa;
+  const struct v7_prop *p2 = *(struct v7_prop **) pb;
+  return cmp(p2->val, p1->val);
+}
+
+V7_PRIVATE enum v7_err Arr_sort(struct v7_c_func_arg *cfa) {
+  int i = 0, length = 0;
+  struct v7_val *v = cfa->this_obj;
+  struct v7_prop *p, **arr;
+
+  /* TODO(lsm): do proper error checking */
+  for (p = v->v.array; p != NULL; p = p->next) {
+    length++;
+  }
+  arr = (struct v7_prop **) malloc(length * sizeof(p));
+  for (i = 0, p = v->v.array; p != NULL; p = p->next) {
+    arr[i++] = p;
+  }
+  qsort(arr, length, sizeof(p), cmp_prop);
+  v->v.array = NULL;
+  for (i = 0; i < length; i++) {
+    arr[i]->next = v->v.array;
+    v->v.array = arr[i];
+  }
+  free(arr);
+  return V7_OK;
+}
+#endif
+
+V7_PRIVATE void init_array(struct v7 *v7) {
+  v7_set_property(v7, v7->global_object, "Array", 5, 0,
+                  v7_create_cfunction(Array_ctor));
+  v7_set_property(v7, v7->array_prototype, "push", 4, 0,
+                  v7_create_cfunction(Array_push));
+}
+/*
+ * Copyright (c) 2014 Cesanta Software Limited
+ * All rights reserved
+ */
+
+
 V7_PRIVATE val_t Boolean_ctor(struct v7 *v7, val_t this_obj, val_t args) {
   val_t v = v7_create_boolean(0);   /* false by default */
   if (v7_is_true(v7, v7_array_at(v7, args, 0))) {
@@ -4502,6 +4572,7 @@ struct v7 *v7_create(void) {
                     v7->global_object);
 
     init_object(v7);
+    init_array(v7);
     init_error(v7);
     init_boolean(v7);
   }
