cesanta/v7

commit cdecb21a4fddda771e4d61ffcd4bd05b4b7295ea

Improve numeric op string conversion

src/interpreter.c
@@ -68,6 +68,7 @@ V7_PRIVATE val_t i_value_of(struct v7 *v7, val_t v) {
 }
 
 V7_PRIVATE double i_as_num(struct v7 *v7, val_t v) {
+  v = i_value_of(v7, v);
   if (!v7_is_double(v) && !v7_is_boolean(v)) {
     if (v7_is_string(v)) {
       double res;
@@ -106,6 +107,33 @@ static double i_num_unary_op(struct v7 *v7, enum ast_tag tag, double a) {
   }
 }
 
+static double i_int_bin_op(struct v7 *v7, enum ast_tag tag, double a,
+                           double b) {
+  switch (tag) {
+    case AST_LSHIFT:
+      return (int) a << (int) b;
+    case AST_RSHIFT:
+      return (int) a >> (int) b;
+    case AST_URSHIFT:
+      return (unsigned int) a >> (int) b;
+    case AST_OR:
+      if (isnan(a)) {
+        a = 0.0;
+      }
+      if (isnan(b)) {
+        b = 0.0;
+      }
+      return (int) a | (int) b;
+    case AST_XOR:
+      return (int) a ^ (int) b;
+    case AST_AND:
+      return (int) a & (int) b;
+    default:
+      throw_exception(v7, "InternalError", "%s", __func__); /* LCOV_EXCL_LINE */
+      return 0;  /* LCOV_EXCL_LINE */
+  }
+}
+
 static double i_num_bin_op(struct v7 *v7, enum ast_tag tag, double a,
                            double b) {
   switch (tag) {
@@ -126,17 +154,14 @@ static double i_num_bin_op(struct v7 *v7, enum ast_tag tag, double a,
       }
       return a / b;
     case AST_LSHIFT:
-      return (int) a << (int) b;
     case AST_RSHIFT:
-      return (int) a >> (int) b;
     case AST_URSHIFT:
-      return (unsigned int) a >> (int) b;
     case AST_OR:
-      return (int) a | (int) b;
     case AST_XOR:
-      return (int) a ^ (int) b;
     case AST_AND:
-      return (int) a & (int) b;
+      return i_int_bin_op(v7, tag,
+                          isnan(a) || isinf(a) ? 0.0 : a,
+                          isnan(b) || isinf(b) ? 0.0 : b);
     default:
       throw_exception(v7, "InternalError", "%s", __func__); /* LCOV_EXCL_LINE */
       return 0;  /* LCOV_EXCL_LINE */

v7.c
@@ -3587,7 +3587,7 @@ static val_t Str_localeCompare(struct v7 *v7, val_t this_obj, val_t args) {
   if (!v7_is_string(arg0) || !v7_is_string(s)) {
     throw_exception(v7, "TypeError", "%s", "string expected");
   } else {
-    res = s_cmp(v7, s, arg0);
+    res = v7_create_boolean(s_cmp(v7, s, arg0));
   }
 
   return res;
@@ -6384,6 +6384,7 @@ V7_PRIVATE val_t i_value_of(struct v7 *v7, val_t v) {
 }
 
 V7_PRIVATE double i_as_num(struct v7 *v7, val_t v) {
+  v = i_value_of(v7, v);
   if (!v7_is_double(v) && !v7_is_boolean(v)) {
     if (v7_is_string(v)) {
       double res;
@@ -6422,6 +6423,33 @@ static double i_num_unary_op(struct v7 *v7, enum ast_tag tag, double a) {
   }
 }
 
+static double i_int_bin_op(struct v7 *v7, enum ast_tag tag, double a,
+                           double b) {
+  switch (tag) {
+    case AST_LSHIFT:
+      return (int) a << (int) b;
+    case AST_RSHIFT:
+      return (int) a >> (int) b;
+    case AST_URSHIFT:
+      return (unsigned int) a >> (int) b;
+    case AST_OR:
+      if (isnan(a)) {
+        a = 0.0;
+      }
+      if (isnan(b)) {
+        b = 0.0;
+      }
+      return (int) a | (int) b;
+    case AST_XOR:
+      return (int) a ^ (int) b;
+    case AST_AND:
+      return (int) a & (int) b;
+    default:
+      throw_exception(v7, "InternalError", "%s", __func__); /* LCOV_EXCL_LINE */
+      return 0;  /* LCOV_EXCL_LINE */
+  }
+}
+
 static double i_num_bin_op(struct v7 *v7, enum ast_tag tag, double a,
                            double b) {
   switch (tag) {
@@ -6442,17 +6470,14 @@ static double i_num_bin_op(struct v7 *v7, enum ast_tag tag, double a,
       }
       return a / b;
     case AST_LSHIFT:
-      return (int) a << (int) b;
     case AST_RSHIFT:
-      return (int) a >> (int) b;
     case AST_URSHIFT:
-      return (unsigned int) a >> (int) b;
     case AST_OR:
-      return (int) a | (int) b;
     case AST_XOR:
-      return (int) a ^ (int) b;
     case AST_AND:
-      return (int) a & (int) b;
+      return i_int_bin_op(v7, tag,
+                          isnan(a) || isinf(a) ? 0.0 : a,
+                          isnan(b) || isinf(b) ? 0.0 : b);
     default:
       throw_exception(v7, "InternalError", "%s", __func__); /* LCOV_EXCL_LINE */
       return 0;  /* LCOV_EXCL_LINE */

src/string.c
@@ -157,7 +157,7 @@ static val_t Str_localeCompare(struct v7 *v7, val_t this_obj, val_t args) {
   if (!v7_is_string(arg0) || !v7_is_string(s)) {
     throw_exception(v7, "TypeError", "%s", "string expected");
   } else {
-    res = s_cmp(v7, s, arg0);
+    res = v7_create_boolean(s_cmp(v7, s, arg0));
   }
 
   return res;

tests/unit_test.c
@@ -756,8 +756,12 @@ static const char *test_ecmac(void) {
   ast_init(&a, 0);
 
   for (i = 0; next_case < db + db_len; i++ ) {
+    char tail_cmd[100];
     char *current_case = next_case + 1;
     ASSERT((next_case = strchr(current_case, '\0')) != NULL);
+    snprintf(tail_cmd, sizeof(tail_cmd),
+             "(tail -c +%lu tests/ecmac.db|head -c %lu)",
+             current_case - db + 1, next_case - current_case);
 
 #if 0
     printf("-- Parsing %d: \"%s\"\n", i, current_case);
@@ -769,20 +773,18 @@ static const char *test_ecmac(void) {
     if ((child = fork()) == 0) {
 #endif
       if (i == 1231 || i == 1250 || i == 1252 || i == 1253 || i == 1251 ||
-          i == 1255 || i == 2649) continue;
-
+          i == 1255 || i == 2649
 #ifndef ECMA_SLOW
-      if (i == 3348 || i == 3349 || i == 3401) continue;
+          || i == 3348 || i == 3349 || i == 3401
 #endif
+          ) {
+        fprintf(r, "%i\tSKIP %s\n", i, tail_cmd);
+        continue;
+      }
 
       if (v7_exec(v7, &res, driver) != V7_OK) {
         fprintf(stderr, "%s: %s\n", "Cannot load ECMA driver", v7->error_msg);
       } else {
-        char tail_cmd[100];
-        snprintf(tail_cmd, sizeof(tail_cmd),
-                 "(tail -c +%lu tests/ecmac.db|head -c %lu)",
-                 current_case - db + 1, next_case - current_case);
-
         if (v7_exec(v7, &res, current_case) != V7_OK) {
           char buf[2048], *err_str = v7_to_json(v7, res, buf, sizeof(buf));
           fprintf(r, "%i\tFAIL %s: [%s]\n", i, tail_cmd, err_str);

commit f9cbb60f165e2216c47dd890e15d3ee02da32bbb

Fix evaluation order of some numeric binary ops
Regression introduced in cdecb21a4fd
when all conversions included an implicit potential
call to `valueOf()`.

Thanks to @giannitedesco for spotting this.

src/interpreter.c
@@ -196,7 +196,7 @@ static val_t i_eval_expr(struct v7 *v7, struct ast *a, ast_off_t *pos,
   const struct ast_node_def *def = &ast_node_defs[tag];
   ast_off_t end;
   val_t res = V7_UNDEFINED, v1, v2;
-  double dv;
+  double d1, d2, dv;
   int i;
 
   /*
@@ -237,10 +237,9 @@ static val_t i_eval_expr(struct v7 *v7, struct ast *a, ast_off_t *pos,
     case AST_OR:
     case AST_XOR:
     case AST_AND:
-      v1 = i_eval_expr(v7, a, pos, scope);
-      v2 = i_eval_expr(v7, a, pos, scope);
-      return v7_create_number(i_num_bin_op(v7, tag, i_as_num(v7, v1),
-                              i_as_num(v7, v2)));
+      d1 = i_as_num(v7, i_eval_expr(v7, a, pos, scope));
+      d2 = i_as_num(v7, i_eval_expr(v7, a, pos, scope));
+      return v7_create_number(i_num_bin_op(v7, tag, d1, d2));
     case AST_EQ_EQ:
       v1 = i_eval_expr(v7, a, pos, scope);
       v2 = i_eval_expr(v7, a, pos, scope);
@@ -425,8 +424,9 @@ static val_t i_eval_expr(struct v7 *v7, struct ast *a, ast_off_t *pos,
           default:
             op = assign_op_map[op - AST_ASSIGN - 1];
             res = i_eval_expr(v7, a, pos, scope);
-            res = v1 = v7_double_to_value(i_num_bin_op(
-                v7, op, i_as_num(v7, v1), i_as_num(v7, res)));
+            d1 = i_as_num(v7, v1);
+            d2 = i_as_num(v7, res);
+            res = v1 = v7_double_to_value(i_num_bin_op(v7, op, d1, d2));
         }
 
         /* variables are modified where they are found in the scope chain */

v7.c
@@ -6557,7 +6557,7 @@ static val_t i_eval_expr(struct v7 *v7, struct ast *a, ast_off_t *pos,
   const struct ast_node_def *def = &ast_node_defs[tag];
   ast_off_t end;
   val_t res = V7_UNDEFINED, v1, v2;
-  double dv;
+  double d1, d2, dv;
   int i;
 
   /*
@@ -6598,10 +6598,9 @@ static val_t i_eval_expr(struct v7 *v7, struct ast *a, ast_off_t *pos,
     case AST_OR:
     case AST_XOR:
     case AST_AND:
-      v1 = i_eval_expr(v7, a, pos, scope);
-      v2 = i_eval_expr(v7, a, pos, scope);
-      return v7_create_number(i_num_bin_op(v7, tag, i_as_num(v7, v1),
-                              i_as_num(v7, v2)));
+      d1 = i_as_num(v7, i_eval_expr(v7, a, pos, scope));
+      d2 = i_as_num(v7, i_eval_expr(v7, a, pos, scope));
+      return v7_create_number(i_num_bin_op(v7, tag, d1, d2));
     case AST_EQ_EQ:
       v1 = i_eval_expr(v7, a, pos, scope);
       v2 = i_eval_expr(v7, a, pos, scope);
@@ -6786,8 +6785,9 @@ static val_t i_eval_expr(struct v7 *v7, struct ast *a, ast_off_t *pos,
           default:
             op = assign_op_map[op - AST_ASSIGN - 1];
             res = i_eval_expr(v7, a, pos, scope);
-            res = v1 = v7_double_to_value(i_num_bin_op(
-                v7, op, i_as_num(v7, v1), i_as_num(v7, res)));
+            d1 = i_as_num(v7, v1);
+            d2 = i_as_num(v7, res);
+            res = v1 = v7_double_to_value(i_num_bin_op(v7, op, d1, d2));
         }
 
         /* variables are modified where they are found in the scope chain */