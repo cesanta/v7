9c1265ee92e61e76724af71e64874d7c9a56e771
Alexander Alashkin <alexander.alashkin@gmail.com>
2015-02-16 19:20:27 +0200

    date.c re-added

--- src/sources.mk
+++ src/sources.mk
@@ -2,6 +2,7 @@ SOURCES = mbuf.c utf.c varint.c tokenizer.c array.c boolean.c math.c string.c \
           ast.c vm.c gc.c parser.c interpreter.c slre.c object.c error.c number.c \
           json.c main.c date.c
 HEADERS = license.h utf.h tokenizer.h mbuf.h ast.h parser.h internal.h vm.h gc.h \
+					slre.h varint.h
 
 OLD_SOURCES = global_vars.c util.c crypto.c regex.c \
               function.c
