a241ee60cd8dc66ffe53811431c74400c3cf534f
Sergey Lyubka <valenok@gmail.com>
2015-02-17 13:31:40 +0000

    Implement Array.prototype.join

--- src/interpreter.c
+++ src/interpreter.c
@@ -659,6 +659,7 @@ static val_t i_eval_expr(struct v7 *v7, struct ast *a, ast_off_t *pos,
           case V7_TYPE_BOOLEAN:
             return v7_create_string(v7, "boolean", 7, 1);
           case V7_TYPE_FUNCTION_OBJECT:
+          case V7_TYPE_CFUNCTION_OBJECT:
           case V7_TYPE_CFUNCTION:
             return v7_create_string(v7, "function", 8, 1);
           default:
