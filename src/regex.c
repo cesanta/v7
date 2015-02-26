798ae1787e78e15245a2bd6deca2d655c4abe352
vrz <vrz@mail.ru>
2015-02-26 14:19:47 +0300

    created functions Regex_exec() & Regex_test()

--- src/regex.c
+++ src/regex.c
@@ -106,77 +106,45 @@ static val_t Regex_set_lastIndex(struct v7 *v7, val_t this_obj, val_t args) {
   return v7_create_number(lastIndex);
 }
 
-#if 0
+static val_t Regex_exec(struct v7 *v7, val_t this_obj, val_t args) {
+  val_t arr = V7_NULL;
+  int num_args = v7_array_length(v7, args);
+  if (v7_is_regexp(this_obj) && num_args > 0) {
+    val_t s = to_string(v7, v7_array_at(v7, args, 0));
+    size_t len;
+    struct slre_loot sub;
+    struct slre_cap *ptok = sub.caps;
+    const char *begin = v7_to_string(v7, &s, &len);
+    struct v7_regexp *rp = v7_to_pointer(this_obj);
+    int flag_g = slre_get_flags(rp->compiled_regexp) & SLRE_FLAG_G;
+    if (rp->lastIndex < 0) rp->lastIndex = 0;
+    if (flag_g) begin = utfnshift(begin, rp->lastIndex);
 
-V7_PRIVATE void Regex_lastIndex(struct v7_val *this_obj, struct v7_val *arg,
-                                struct v7_val *result) {
-  if (arg)
-    this_obj->v.str.lastIndex = arg->v.num;
-  else
-    v7_init_num(result, this_obj->v.str.lastIndex);
-}
-
-V7_PRIVATE enum v7_err regex_check_prog(struct v7_val *re_obj) {
-  if (re_obj->v.str.prog == NULL) {
-    int res = slre_compile(re_obj->v.str.buf, &re_obj->v.str.prog);
-    if (res != SLRE_OK) {
-      return V7_REGEXP_ERROR;
-    } else if (re_obj->v.str.prog == NULL) {
-      return V7_OUT_OF_MEMORY;
-    }
-  }
-  return V7_OK;
-}
-
-V7_PRIVATE enum v7_err Regex_exec(struct v7_c_func_arg *cfa) {
-#define v7 (cfa->v7) /* Needed for TRY() macro below */
-  struct v7_val *arg = cfa->args[0], *arr = NULL, *t = cfa->this_obj;
-  struct slre_loot sub;
-  struct slre_cap *ptok = sub.caps;
-
-  if (cfa->num_args > 0) {
-    char *begin;
-    TRY(check_str_re_conv(v7, &arg, 0));
-    begin = arg->v.str.buf;
-    if (t->v.str.lastIndex < 0) t->v.str.lastIndex = 0;
-    if (t->fl.fl.re_flags & SLRE_FLAG_G) {
-      begin = utfnshift(begin, t->v.str.lastIndex);
-    }
-    TRY(regex_check_prog(t));
-    if (!slre_exec(t->v.str.prog, t->fl.fl.re_flags, begin, &sub)) {
+    if (!slre_exec(rp->compiled_regexp, begin, len, &sub)) {
       int i;
-      arr = v7_push_new_object(v7);
-      v7_set_class(arr, V7_CLASS_ARRAY);
+      arr = v7_create_array(v7);
+      
       for (i = 0; i < sub.num_captures; i++, ptok++)
-        v7_append(v7, arr, v7_mkv(v7, V7_TYPE_STR, ptok->start,
-                                  ptok->end - ptok->start, 1));
-      if (t->fl.fl.re_flags & SLRE_FLAG_G)
-        t->v.str.lastIndex = utfnlen(begin, sub.caps->end - begin);
-      return V7_OK;
-    } else {
-      t->v.str.lastIndex = 0;
-    }
+        v7_array_append(v7, arr, v7_create_string(v7, ptok->start, ptok->end - ptok->start, 1));
+      if (flag_g) rp->lastIndex = utfnlen(begin, sub.caps->end - begin);
+    } else rp->lastIndex = 0;
   }
-  TRY(v7_make_and_push(v7, V7_TYPE_NULL));
-  return V7_OK;
-#undef v7
+  return arr;
 }
 
-V7_PRIVATE enum v7_err Regex_test(struct v7_c_func_arg *cfa) {
-#define v7 (cfa->v7) /* Needed for TRY() macro below */
-  TRY(Regex_exec(cfa));
-
-  v7_push_bool(v7, v7_top_val(v7)->type != V7_TYPE_NULL);
-  return V7_OK;
-#undef v7
+static val_t Regex_test(struct v7 *v7, val_t this_obj, val_t args) {
+  return v7_create_boolean(Regex_exec(v7, this_obj, args) != V7_NULL);
 }
-#endif
+
 V7_PRIVATE void init_regex(struct v7 *v7) {
   val_t ctor = v7_create_cfunction(Regex_ctor);
   val_t lastIndex = v7_create_array(v7);
 
   v7_set_property(v7, v7->global_object, "RegExp", 6, 0, ctor);
   v7_set(v7, v7->regexp_prototype, "constructor", 11, ctor);
+
+  set_cfunc_prop(v7, v7->regexp_prototype, "exec", Regex_exec);
+  set_cfunc_prop(v7, v7->regexp_prototype, "test", Regex_test);
 
   v7_set_property(v7, v7->regexp_prototype, "global", 6, V7_PROPERTY_GETTER,
                   v7_create_cfunction(Regex_global));
@@ -191,11 +159,4 @@ V7_PRIVATE void init_regex(struct v7 *v7) {
   v7_set(v7, lastIndex, "1", 1, v7_create_cfunction(Regex_set_lastIndex));
   v7_set_property(v7, v7->regexp_prototype, "lastIndex", 9,
                   V7_PROPERTY_GETTER | V7_PROPERTY_SETTER, lastIndex);
-
-/* init_standard_constructor(V7_CLASS_REGEXP, Regex_ctor);
-
-  SET_METHOD(s_prototypes[V7_CLASS_REGEXP], "exec", Regex_exec);
-  SET_METHOD(s_prototypes[V7_CLASS_REGEXP], "test", Regex_test);
-
-  SET_RO_PROP_V(s_global, "RegExp", s_constructors[V7_CLASS_REGEXP]); */
 }
