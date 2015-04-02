1b2355a0ed0e81957127e667a9f4516eefa88e61
rojer <rojer@rojer.me>
2015-04-02 15:42:32 +0100

    Introduce feature selection machinery
    Sets of features will be called "profiles" and we'll start with 3:
    minimal, medium and full (the default).
    Features are enabled and disabled by turning the knobs on and off.
    Feature knobs follow common naming format: V7_ENABLE_Foo__bar (note the
    double underscore), where "Foo" can be loosely defined as a "module" and
    "bar" as a feature within it. "Feature" can be as small as a single
    function or method or a collection of closely related functions/methods.
    Slicing of the Date included in this commit illustrates intended use.
    
    Full profile has all the features enabled automatically by grepping for
    all the feature names and turning them on, other profiles will be
    defined manually (current definitions are just skeletons).

--- src/sources.mk
+++ src/sources.mk
@@ -2,7 +2,8 @@ SOURCES = mbuf.c utf.c varint.c tokenizer.c array.c boolean.c math.c string.c \
           ast.c vm.c gc.c parser.c interpreter.c slre.c object.c error.c \
           number.c json.c main.c date.c function.c stdlib.c js_stdlib.c \
           regex.c socket.c os.c
-HEADERS = license.h utf.h tokenizer.h mbuf.h ast.h parser.h mm.h internal.h \
+HEADERS = features_full.h features_medium.h features_minimal.h \
+          license.h utf.h tokenizer.h mbuf.h ast.h parser.h mm.h internal.h \
           vm.h gc.h slre.h varint.h
 
 OLD_SOURCES = global_vars.c util.c crypto.c
