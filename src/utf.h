1fcbff49d51ff532bfc51ea52a40bf1856c897d9
Deomid Ryabkov <rojer@cesanta.com>
2015-04-05 20:03:45 +0100

    Reduce UTF library footprint to the bare minimum
    Leave only the functions currently used.

--- src/utf.h
+++ src/utf.h
@@ -33,33 +33,36 @@ int iswordchar(Rune c);
 int isalpharune(Rune c);
 int islowerrune(Rune c);
 int isspacerune(Rune c);
-int istitlerune(Rune c);
 int isupperrune(Rune c);
+int runetochar(char *str, Rune *rune);
+Rune tolowerrune(Rune c);
+Rune toupperrune(Rune c);
+int utflen(char *s);
+int utfnlen(char *s, long m);
+char *utfnshift(char *s, long m);
+
+#if 0 /* Not implemented. */
+int istitlerune(Rune c);
 int runelen(Rune c);
 int runenlen(Rune *r, int nrune);
 Rune *runestrcat(Rune *s1, Rune *s2);
 Rune *runestrchr(Rune *s, Rune c);
-int runestrcmp(Rune *s1, Rune *s2);
 Rune *runestrcpy(Rune *s1, Rune *s2);
 Rune *runestrdup(Rune *s);
 Rune *runestrecpy(Rune *s1, Rune *es1, Rune *s2);
+int runestrcmp(Rune *s1, Rune *s2);
 long runestrlen(Rune *s);
 Rune *runestrncat(Rune *s1, Rune *s2, long n);
 int runestrncmp(Rune *s1, Rune *s2, long n);
 Rune *runestrncpy(Rune *s1, Rune *s2, long n);
 Rune *runestrrchr(Rune *s, Rune c);
 Rune *runestrstr(Rune *s1, Rune *s2);
-int runetochar(char *str, Rune *rune);
-Rune tolowerrune(Rune c);
 Rune totitlerune(Rune c);
-Rune toupperrune(Rune c);
 char *utfecpy(char *to, char *e, char *from);
-int utflen(char *s);
-int utfnlen(char *s, long m);
-char *utfnshift(char *s, long m);
 char *utfrrune(char *s, long c);
 char *utfrune(char *s, long c);
 char *utfutf(char *s1, char *s2);
+#endif
 
 #if defined(__cplusplus)
 }
