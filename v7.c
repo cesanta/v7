872f661edad61acd41cb0d53c22114153be297bc
Sergey Lyubka <lsm@cesanta.com>
2015-04-29 11:31:12 +0100

    Move utf.[ch] to common/
        PUBLISHED_FROM=7eff42ddb06209401106953fa0075a2b3e3b48e2

--- v7.c
+++ v7.c
@@ -388,76 +388,6 @@ int v7_main(int argc, char *argv[], void (*init_func)(struct v7 *));
  * All rights reserved
  */
 
-#ifndef _UTF_H_
-#define _UTF_H_ 1
-
-#if defined(__cplusplus)
-extern "C" {
-#endif /* __cplusplus */
-
-typedef unsigned char uchar;
-
-typedef unsigned short Rune; /* 16 bits */
-
-#define nelem(a) (sizeof(a) / sizeof(a)[0])
-
-enum {
-  UTFmax = 3,               /* maximum bytes per rune */
-  Runesync = 0x80,          /* cannot represent part of a UTF sequence (<) */
-  Runeself = 0x80,          /* rune and UTF sequences are the same (<) */
-  Runeerror = 0xFFFD        /* decoding error in UTF */
-  /* Runemax    = 0xFFFC */ /* maximum rune value */
-};
-
-/* Edit .+1,/^$/ | cfn $PLAN9/src/lib9/utf/?*.c | grep -v static |grep -v __ */
-int chartorune(Rune *rune, const char *str);
-int fullrune(char *str, int n);
-int isdigitrune(Rune c);
-int isnewline(Rune c);
-int iswordchar(Rune c);
-int isalpharune(Rune c);
-int islowerrune(Rune c);
-int isspacerune(Rune c);
-int isupperrune(Rune c);
-int runetochar(char *str, Rune *rune);
-Rune tolowerrune(Rune c);
-Rune toupperrune(Rune c);
-int utfnlen(char *s, long m);
-char *utfnshift(char *s, long m);
-
-#if 0 /* Not implemented. */
-int istitlerune(Rune c);
-int runelen(Rune c);
-int runenlen(Rune *r, int nrune);
-Rune *runestrcat(Rune *s1, Rune *s2);
-Rune *runestrchr(Rune *s, Rune c);
-Rune *runestrcpy(Rune *s1, Rune *s2);
-Rune *runestrdup(Rune *s);
-Rune *runestrecpy(Rune *s1, Rune *es1, Rune *s2);
-int runestrcmp(Rune *s1, Rune *s2);
-long runestrlen(Rune *s);
-Rune *runestrncat(Rune *s1, Rune *s2, long n);
-int runestrncmp(Rune *s1, Rune *s2, long n);
-Rune *runestrncpy(Rune *s1, Rune *s2, long n);
-Rune *runestrrchr(Rune *s, Rune c);
-Rune *runestrstr(Rune *s1, Rune *s2);
-Rune totitlerune(Rune c);
-char *utfecpy(char *to, char *e, char *from);
-int utflen(char *s);
-char *utfrrune(char *s, long c);
-char *utfrune(char *s, long c);
-char *utfutf(char *s1, char *s2);
-#endif
-
-#if defined(__cplusplus)
-}
-#endif /* __cplusplus */
-#endif /* _UTF_H_ */
-/*
- * Copyright (c) 2014 Cesanta Software Limited
- * All rights reserved
- */
-
 #ifndef V7_TOKENIZER_H_INCLUDED
 #define V7_TOKENIZER_H_INCLUDED
 
@@ -669,6 +599,76 @@ void mbuf_trim(struct mbuf *);
 #endif /* __cplusplus */
 
 #endif /* MBUF_H_INCLUDED */
+/*
+ * Copyright (c) 2014 Cesanta Software Limited
+ * All rights reserved
+ */
+
+#ifndef _UTF_H_
+#define _UTF_H_ 1
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /* __cplusplus */
+
+typedef unsigned char uchar;
+
+typedef unsigned short Rune; /* 16 bits */
+
+#define nelem(a) (sizeof(a) / sizeof(a)[0])
+
+enum {
+  UTFmax = 3,               /* maximum bytes per rune */
+  Runesync = 0x80,          /* cannot represent part of a UTF sequence (<) */
+  Runeself = 0x80,          /* rune and UTF sequences are the same (<) */
+  Runeerror = 0xFFFD        /* decoding error in UTF */
+  /* Runemax    = 0xFFFC */ /* maximum rune value */
+};
+
+/* Edit .+1,/^$/ | cfn $PLAN9/src/lib9/utf/?*.c | grep -v static |grep -v __ */
+int chartorune(Rune *rune, const char *str);
+int fullrune(char *str, int n);
+int isdigitrune(Rune c);
+int isnewline(Rune c);
+int iswordchar(Rune c);
+int isalpharune(Rune c);
+int islowerrune(Rune c);
+int isspacerune(Rune c);
+int isupperrune(Rune c);
+int runetochar(char *str, Rune *rune);
+Rune tolowerrune(Rune c);
+Rune toupperrune(Rune c);
+int utfnlen(char *s, long m);
+char *utfnshift(char *s, long m);
+
+#if 0 /* Not implemented. */
+int istitlerune(Rune c);
+int runelen(Rune c);
+int runenlen(Rune *r, int nrune);
+Rune *runestrcat(Rune *s1, Rune *s2);
+Rune *runestrchr(Rune *s, Rune c);
+Rune *runestrcpy(Rune *s1, Rune *s2);
+Rune *runestrdup(Rune *s);
+Rune *runestrecpy(Rune *s1, Rune *es1, Rune *s2);
+int runestrcmp(Rune *s1, Rune *s2);
+long runestrlen(Rune *s);
+Rune *runestrncat(Rune *s1, Rune *s2, long n);
+int runestrncmp(Rune *s1, Rune *s2, long n);
+Rune *runestrncpy(Rune *s1, Rune *s2, long n);
+Rune *runestrrchr(Rune *s, Rune c);
+Rune *runestrstr(Rune *s1, Rune *s2);
+Rune totitlerune(Rune c);
+char *utfecpy(char *to, char *e, char *from);
+int utflen(char *s);
+char *utfrrune(char *s, long c);
+char *utfrune(char *s, long c);
+char *utfutf(char *s1, char *s2);
+#endif
+
+#if defined(__cplusplus)
+}
+#endif /* __cplusplus */
+#endif /* _UTF_H_ */
 /*
  * Copyright (c) 2014 Cesanta Software Limited
  * All rights reserved
