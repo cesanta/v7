fb16bcf195ca21b197558afea1bf65b72c743677
MURAOKA Taro <koron.kaoriya@gmail.com>
2015-08-15 05:54:54 +0900

    to be compiled with 64bit VisualStudio 2010

--- v7.c
+++ v7.c
@@ -1604,7 +1604,9 @@ typedef int int32_t;
 typedef unsigned int uint32_t;
 typedef unsigned short uint16_t;
 typedef unsigned char uint8_t;
+#ifndef _UINTPTR_T_DEFINED
 typedef unsigned long uintptr_t;
+#endif
 #define __func__ ""
 #else
 #include <stdint.h>
