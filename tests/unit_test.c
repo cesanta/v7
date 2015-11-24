3b260785837490bed8e6bb28d30389db59b79e1f
Marko Mikulicic <mkm@cesanta.com>
2015-11-24 04:38:55 +0300

    Fix other non taken loops in bcode
        PUBLISHED_FROM=a985cd598108cab897474a17a169f6ae47f38850

--- tests/unit_test.c
+++ tests/unit_test.c
@@ -2361,10 +2361,12 @@ static const char *test_exec_bcode(void) {
   ASSERT_BCODE_EVAL_NUM_EQ(v7, "a=1;while(a) a-=1", 0);
   ASSERT_BCODE_EVAL_NUM_EQ(v7, "a=3;b=0;while(a) {a-=1;b+=1}", 3);
   ASSERT_BCODE_EVAL_NUM_EQ(v7, "a=3;b=0;while(a) {a-=1;b+=1};b", 3);
+  ASSERT_BCODE_EVAL_NUM_EQ(v7, "1; while(false) {}", 1);
 
   ASSERT_BCODE_EVAL_NUM_EQ(v7, "a=0;b=0;for(i=0;i<10;i+=1) b+=1", 10);
   ASSERT_BCODE_EVAL_EQ(v7, "for(1;false;1) 1", "undefined");
   ASSERT_BCODE_EVAL_EQ(v7, "for(1;false;) 1", "undefined");
+  ASSERT_BCODE_EVAL_NUM_EQ(v7, "1; for(;false;) {}", 1);
 
   ASSERT_BCODE_EVAL_EQ(v7, "!0", "true");
   ASSERT_BCODE_EVAL_NUM_EQ(v7, "~0", -1);
